6da251cf83edf73b94c7edc53f4745179d18d887
Derek Gomez <demagome@ucsc.edu>
2024-10-10 23:35:12 -0700

    create_output_file_name works! I was getting screwed over by \r >:( so I wrote remove_invisible_chars :))

--- dir_deploy.sh
+++ dir_deploy.sh
@@ -13,6 +13,13 @@ output_dir=""
 original_string=""
 replacement_string=""
 
+function remove_invisible_chars() {
+    local input="$1"
+    # Use 'tr' to remove non-printable characters and control characters
+    # but keep spaces
+    echo "$input" | tr -cd '[:print:][:space:]'
+}
+
 # extract the values from the args file and assign them to the args array
 function extract_args {
     # read through the args file
@@ -40,18 +47,35 @@ function assign_args {
     replacement_string="${args[3]}"
 }
 
+# remove invisible characters from the args
+function clean_args {
+    input_dir="$(remove_invisible_chars "$input_dir")"
+    output_dir="$(remove_invisible_chars "$output_dir")"
+    original_string="$(remove_invisible_chars "$original_string")"
+    replacement_string="$(remove_invisible_chars "$replacement_string")"
+}
+
 #
 # $1 filename from input_dir
 function create_output_file_name {
-    # input_dir/*/filename -> output_dir/*/filename
-    # output_dir + / + $1.split(/)[1, ...]
     # split the input's filename by the first "/"
-    IFS="/" read -r 1st_part 2nd_part <<< "$1"
+    IFS="/" read -r first_part second_part <<< "$1"
+    
+    local output_file_name=""$output_dir"/"$second_part""
+
+    # echo "running create_output_file_name..."
+    # echo -E "input_file_name: $1"
+    # echo -E "first_input: $first_part"
+    # echo -E "second_part: $second_part"
+    # echo -E "output_dir: $output_dir"
+    # declare -p output_dir
+    echo -E "output_file_name: "$output_file_name""
 }
 
 # function calls
 extract_args
 assign_args
+clean_args
 
 echo "
 _______ARGUMENTS_______
@@ -61,20 +85,23 @@ original_string: "$original_string"
 replacement_string: "$replacement_string"
 "
 
-echo "
-___Directories before replacement___
-input_dir:"
-ls input_dir
-echo "
-output_dir:"
-ls output_dir
+# echo "
+# ___Directories before replacement___
+# input_dir:"
+# ls input_dir
+# echo "
+# output_dir:"
+# ls output_dir
 
+# output directory should mirror the input directory
+# files can be empty
 
+create_output_file_name input_dir/input.txt
 
-echo "
-___Directories after replacement___
-input_dir:"
-ls input_dir
-echo "
-output_dir:"
-ls output_dir
+# echo "
+# ___Directories after replacement___
+# input_dir:"
+# ls input_dir
+# echo "
+# output_dir:"
+# ls output_dir
