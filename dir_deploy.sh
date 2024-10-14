3bd671e6bf08daff8bc20c705b09f6de1e76ea8c
Derek Gomez <demagome@ucsc.edu>
2024-10-14 05:09:51 -0700

    it works. I did it. we have recursive substring replacement now it just needs to be cleaned up

--- dir_deploy.sh
+++ dir_deploy.sh
@@ -97,7 +97,59 @@ function fileOrDirectory {
     fi
 }
 
+# replace_substring and create_new_copy directly copied from final.sh
+
+# $1 full string
+# $2 substring to replace
+# $3 replacement to string
+function replace_substring {
+    local original_string="$1"
+    local substring_to_replace="$2"
+    local replacement_string="$3"
+
+    # perform the replacement using parameter expansion
+    local new_string="${original_string//$substring_to_replace/$replacement_string}"
+
+    # return
+    echo "$new_string"
+}
+
+
+# rewrite this so that it takes arguments instead
+# $1 input_file
+# $2 output_file
+function create_new_copy {
+    local input_file=$1
+    local output_file=$2
+
+    # check if the input file exists
+    if [[ -f "$input_file" ]]; then
+        # empty or create the output file
+        > "$output_file"
+
+        # read through input_file, and write to output_file
+        LINE=1
+        while read -r CURRENT_LINE || [[ -n "$CURRENT_LINE" ]]; do
+            # perform the substring replacement
+            modified_line=$(replace_substring "$CURRENT_LINE" "$original_string" "$replacement_string")
+
+            # write the modified line to the output file
+            echo "$modified_line" >> "$output_file"
+            ((LINE++))
+        done < "$input_file"
+
+        # echo "File processed. New file saved to $output_file"
+        
+    # input file doesn't exist
+    else
+        echo "Input file does not exist: $input_file"
+        echo "Double-check your file path"
+    fi
+}
+
+
 # recursively navigate through directories and subdirectories
+# execute substring replacement as we clone from input_dir to output_dir
 # $1 directory path
 main() {
     local dir="$1"
@@ -111,14 +163,18 @@ main() {
 
             # If the item is a directory, recursively call the function
             if [ -d "$item" ]; then
-                # traverse_directory "$item"
-                mkdir $(create_output_file_name "$item") # create the output directory clone
-                main "$item"                             # navigate int
+                mkdir $(create_output_file_name "$item") # create the output_directory clone
+                main "$item"                             # navigate into the directory
 
             # If the item is a file, print its name
             elif [ -f "$item" ]; then
                 # echo -e "\nFile: $item"
-                touch $(create_output_file_name "$item") # create the output file clone
+
+                # create the output_file clone
+                local output_clone=$(create_output_file_name "$item")
+                # touch "$output_clone"
+                create_new_copy "$item" "$output_clone"
+
             fi
         done
     else
